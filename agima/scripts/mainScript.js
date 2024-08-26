3cb04c26e2db06f96a76a093690f1887077d764e
EugeniiSh <evgenii.shmerko@mail.ru>
2024-08-26 15:06:30 +0300

    feat: add main logic

--- agima/scripts/mainScript.js
+++ agima/scripts/mainScript.js
@@ -1,5 +1,6 @@
 import { Slider } from './slider.js';
 
+/* +++ === Slider === +++*/
 const slider = document.querySelector('.slider-container');
 
 const sliderSetup = 
@@ -17,3 +18,82 @@ const sliderSetup =
 }
 
 const sliderObj = new Slider(sliderSetup);
+/* --- === Slider === ---*/
+
+/* +++ === Header === +++*/
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () =>
+{
+  const header = document.querySelector('.header');
+  header.classList.toggle('header--active');
+
+  const main = document.querySelector('.main');
+  main.classList.toggle('main__is-header--active');
+  main.classList.toggle('main--active');
+})
+/* --- === Header === ---*/
+
+/* +++ === Progress bar === +++*/
+function progressBar() 
+{
+  // Узнаем на сколько страница прокручена
+  const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  // Узнаем высоту всей страницы
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  // Получаем в % на сколько прокручена страница
+  const scrolled = scroll / height * 100;
+
+  // Подставляем % прокрутки в ширину нашей линии
+  document.querySelector('.progress-bar__line').style.width = scrolled + '%';
+}
+
+// Запускаем функцию, когда пользователь скролит
+window.addEventListener('scroll', progressBar);
+/* --- === Progress bar === ---*/
+
+/* +++ === Navigation backlight === +++*/
+function getId(link)
+{
+  return link.getAttribute('href').replace('#', '');
+}
+
+const observer = new IntersectionObserver((entries) =>
+{
+  entries.forEach((entry) =>
+  {
+    if(entry.isIntersecting)
+    {
+      document.querySelectorAll('.content-item').forEach((link) =>
+      {
+        link.querySelector('.item-link__text').classList.toggle(
+          'item-link__text--active', 
+          getId(link) === entry.target.id
+        )
+      })
+    }
+  })
+}, 
+{
+  threshold: 0.7,
+});
+
+document.querySelectorAll('.section').forEach((section) => observer.observe(section))
+
+document.querySelector('.content-list').addEventListener('click', (event) =>
+{
+  if(event.target.classList.contains('content-item'))
+  {
+    event.preventDefault();
+
+    const id = getId(event.target);
+
+    window.scrollTo(
+      {
+        top: document.getElementById(id).offsetTop,
+        behavior: 'smooth',
+      }
+    );
+  }
+});
+/* --- === Navigation backlight === ---*/
